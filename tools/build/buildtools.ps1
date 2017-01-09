86213bc539a873d6c7a63b0b88442d4ecc67b85c
realvizu <vizkeletiferenc@gmail.com>
2017-01-09 08:58:55 +0100

    Adding ps script Set-VsixVersion.

---
+++ tools/build/buildtools.ps1
@@ -0,0 +1,25 @@
+﻿ function Set-VsixVersion {
+    [cmdletbinding()]
+    param (
+        [Parameter(Mandatory=1)]
+        [string]$vsixmanifestPath,
+
+        [Parameter(Mandatory=1)]
+        [string]$version
+    )
+    process {
+        Write-Host "Setting version in:" $vsixmanifestPath "to" $version "..." -NoNewline
+
+        [xml]$vsixmanifestXml = Get-Content $vsixmanifestPath
+        
+        $namespaceManager = New-Object System.Xml.XmlNamespaceManager $vsixmanifestXml.NameTable
+        $namespaceManager.AddNamespace("ns", $vsixmanifestXml.DocumentElement.NamespaceURI) | Out-Null
+
+        $versionAttribute = $vsixmanifestXml.SelectSingleNode("//ns:Identity", $namespaceManager).Attributes["Version"]
+        $versionAttribute.Value = $version
+
+        $vsixmanifestXml.Save($vsixmanifestPath)
+
+        Write-Host " Done."
+    }
+}
