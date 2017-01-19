b74a4df83748c617e150c57af5ca10a1e2b9d394
Ferenc Vizkeleti <realvizu@users.noreply.github.com>
2017-01-19 14:30:58 +0100

    Update buildtools.ps1

--- tools/build/buildtools.ps1
+++ tools/build/buildtools.ps1
@@ -10,15 +10,9 @@
     process {
         Write-Host "Setting version in:" $vsixmanifestPath "to" $version "..." -NoNewline
 
-        [xml]$vsixmanifestXml = Get-Content $vsixmanifestPath
-        
-        $namespaceManager = New-Object System.Xml.XmlNamespaceManager $vsixmanifestXml.NameTable
-        $namespaceManager.AddNamespace("ns", $vsixmanifestXml.DocumentElement.NamespaceURI) | Out-Null
-
-        $versionAttribute = $vsixmanifestXml.SelectSingleNode("//ns:Identity", $namespaceManager).Attributes["Version"]
-        $versionAttribute.Value = $version
-
-        $vsixmanifestXml.Save($vsixmanifestPath)
+	    [xml]$xmlContent = Get-Content $vsixmanifestPath
+	    $xmlContent.PackageManifest.Metadata.Identity.Version = $version
+	    $xmlContent.Save($vsixmanifestPath)
 
         Write-Host "Done."
     }
