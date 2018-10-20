c752c3d2c298caa6306be5d7b8f6f4dcd6fb574b
realvizu <vizkeletiferenc@gmail.com>
2018-10-20 09:32:25 +0200

    Updated VisualStudioIntegration project to new project file format.

--- source/Codartis.SoftVis.VisualStudioIntegration.UnitTests/Util/XmlDocumentationTests.cs
+++ source/Codartis.SoftVis.VisualStudioIntegration.UnitTests/Util/XmlDocumentationTests.cs
@@ -12,7 +12,7 @@ namespace Codartis.SoftVis.VisualStudioIntegration.UnitTests.Util
         public void Create_NonXmlInput_Exception()
         {
             Action a = () => new XmlDocumentation("kutya");
-            a.ShouldThrow<XmlException>();
+            a.Should().Throw<XmlException>();
         }
 
         [Fact]
@@ -31,21 +31,21 @@ namespace Codartis.SoftVis.VisualStudioIntegration.UnitTests.Util
         public void GetSummary_NonEmptyInput_Works()
         {
             const string xmlString = "<doc><summary>UnitTest</summary></doc>";
-            new XmlDocumentation(xmlString).Summary.ShouldBeEquivalentTo("UnitTest");
+            new XmlDocumentation(xmlString).Summary.Should().BeEquivalentTo("UnitTest");
         }
 
         [Fact]
         public void GetSummary_XmlWithTyperefs_Works()
         {
             const string xmlString = @"<doc><summary>Unit <see cref=""T:System.IO.FileInfo""/> test</summary></doc>";
-            new XmlDocumentation(xmlString).Summary.ShouldBeEquivalentTo("Unit System.IO.FileInfo test");
+            new XmlDocumentation(xmlString).Summary.Should().BeEquivalentTo("Unit System.IO.FileInfo test");
         }
 
         [Fact]
         public void GetSummary_MultipleWhitespacesReplacedWithSingleSpace()
         {
             const string xmlString = "<doc><summary>Unit  \n\r\t  \t \n \rtest</summary></doc>";
-            new XmlDocumentation(xmlString).Summary.ShouldBeEquivalentTo("Unit test");
+            new XmlDocumentation(xmlString).Summary.Should().BeEquivalentTo("Unit test");
         }
     }
 }
