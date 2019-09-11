c563d294ede26f2f65f6653b8da99d83a97025ec
realvizu <vizkeletiferenc@gmail.com>
2019-09-11 14:41:40 +0200

    Unit tests for DiagramNode.Rect.

---
+++ source/Codartis.SoftVis.UnitTests/Diagramming/Implementation/DiagramNodeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Codartis.SoftVis.Diagramming.Implementation;
+using Codartis.SoftVis.Geometry;
+using Codartis.SoftVis.Modeling.Definition;
+using Codartis.SoftVis.Modeling.Implementation;
+using Codartis.Util;
+using FluentAssertions;
+using Xunit;
+
+namespace Codartis.SoftVis.UnitTests.Diagramming.Implementation
+{
+    public class DiagramNodeTests
+    {
+        [Fact]
+        public void Rect_IsCalculatedCorrectly()
+        {
+            var modelNode = new ModelNode(ModelNodeId.Create(), "A", ModelNodeStereotype.Default);
+            var diagramNode = new DiagramNode(
+                modelNode,
+                DateTime.Now,
+                new Point2D(1, 2),
+                new Size2D(3, 4),
+                new Size2D(5, 6),
+                Maybe<ModelNodeId>.Nothing);
+
+            diagramNode.Rect.Should().Be(new Rect2D(1, 2, 1 + 5, 2 + 10));
+        }
+    }
+}
