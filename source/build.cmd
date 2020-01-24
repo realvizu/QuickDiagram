5d9667636fe155b8a46eae5a7da57c8bdd1e0390
realvizu <vizkeletiferenc@gmail.com>
2020-01-24 08:58:16 +0100

    Updated VSSDK to 15.x to produce a valid VSIX. Updated build.cmd result path.

--- source/build.cmd
+++ source/build.cmd
@@ -1,7 +1,7 @@
 rd /S /Q out
 msbuild.exe QuickDiagramTool.sln /t:Clean;Build /p:Configuration=Release
 md out
-xcopy SoftVis.VisualStudioIntegration\bin\Release\QuickDiagramTool.vsix out
+xcopy SoftVis.VisualStudioIntegration\bin\Release\net46\QuickDiagramTool.vsix out
 
 
 
