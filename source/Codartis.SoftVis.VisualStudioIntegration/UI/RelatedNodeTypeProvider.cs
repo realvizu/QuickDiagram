7d212906b47b14e07a911dbfa8e7e7d400d26961
realvizu <vizkeletiferenc@gmail.com>
2020-03-06 08:26:18 +0100

    Updating RelatedNodeTypeProvider.

--- source/Codartis.SoftVis.VisualStudioIntegration/UI/RelatedNodeTypeProvider.cs
+++ source/Codartis.SoftVis.VisualStudioIntegration/UI/RelatedNodeTypeProvider.cs
@@ -2,26 +2,57 @@
 using Codartis.SoftVis.Modeling.Definition;
 using Codartis.SoftVis.UI;
 using Codartis.SoftVis.VisualStudioIntegration.Modeling;
+using Codartis.Util;
+using JetBrains.Annotations;
 
 namespace Codartis.SoftVis.VisualStudioIntegration.UI
 {
     public sealed class RelatedNodeTypeProvider : IRelatedNodeTypeProvider
     {
+        [NotNull]
+        private static readonly ModelNodeStereotype[] TypesWithMembersStereotypes =
+        {
+            ModelNodeStereotypes.Class,
+            ModelNodeStereotypes.Interface,
+            ModelNodeStereotypes.Class,
+            ModelNodeStereotypes.Enum
+        };
+
+        //[NotNull]
+        //private static readonly ModelNodeStereotype[] MemberStereotypes =
+        //{
+        //    ModelNodeStereotypes.Event,
+        //    ModelNodeStereotypes.Field,
+        //    ModelNodeStereotypes.Method,
+        //    ModelNodeStereotypes.Property
+        //};
+
         public IEnumerable<RelatedNodeType> GetRelatedNodeTypes(ModelNodeStereotype stereotype)
         {
-            if (stereotype.Equals(ModelNodeStereotypes.Property))
+            //yield return new RelatedNodeType(CommonDirectedModelRelationshipTypes.Container, "Parent");
+            
+            if (stereotype.In(TypesWithMembersStereotypes))
             {
-                yield return new RelatedNodeType(DirectedModelRelationshipTypes.AssociatedType, "Type");
+                yield return new RelatedNodeType(CommonDirectedModelRelationshipTypes.Contained, "Members");
+                yield return new RelatedNodeType(DirectedModelRelationshipTypes.AssociatedType, "Outgoing Associations");
+                yield return new RelatedNodeType(DirectedModelRelationshipTypes.AssociatingType, "Incoming Associations");
             }
-            else if (stereotype.Equals(ModelNodeStereotypes.Class) ||
-                     stereotype.Equals(ModelNodeStereotypes.Interface))
+
+            if (stereotype.Equals(ModelNodeStereotypes.Class))
+            {
+                yield return new RelatedNodeType(DirectedModelRelationshipTypes.BaseType, "Base types");
+                yield return new RelatedNodeType(DirectedModelRelationshipTypes.Subtype, "Subtypes");
+                yield return new RelatedNodeType(DirectedModelRelationshipTypes.ImplementedInterface, "Interfaces");
+            }
+            else if (stereotype.Equals(ModelNodeStereotypes.Interface))
             {
                 yield return new RelatedNodeType(DirectedModelRelationshipTypes.BaseType, "Base types");
                 yield return new RelatedNodeType(DirectedModelRelationshipTypes.Subtype, "Subtypes");
                 yield return new RelatedNodeType(DirectedModelRelationshipTypes.ImplementerType, "Implementers");
+            }
+            else if (stereotype.Equals(ModelNodeStereotypes.Struct))
+            {
                 yield return new RelatedNodeType(DirectedModelRelationshipTypes.ImplementedInterface, "Interfaces");
-                yield return new RelatedNodeType(CommonDirectedModelRelationshipTypes.Container, "Parent");
-                yield return new RelatedNodeType(CommonDirectedModelRelationshipTypes.Contained, "Members");
             }
         }
     }
